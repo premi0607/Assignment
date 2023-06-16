3546e54ab972b675f1236431b43b0168a97e027f
premiikha_s <spremikhha@gmail.com>
2023-06-16 03:06:51 -0700

    display-card

--- transaction/src/app/display-expense/display-expense.component.ts
+++ transaction/src/app/display-expense/display-expense.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranserviceService } from '../transervice.service';
 
 @Component({
   selector: 'app-display-expense',
@@ -9,9 +11,15 @@ import { Component } from '@angular/core';
 export class DisplayExpenseComponent {
 
   expense:any=[];
+  DelId:any;
+  newmovie:any={
+    "name": "",
+    "amount": 0,
+    "date": 0,
+    "desc": ""
+  }
 
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,  private router:Router, private transService: TranserviceService) {}
   ngOnInit() {
     this.http
       .get('https://648a952117f1536d65e94eb1.mockapi.io/expense')
@@ -19,6 +27,48 @@ export class DisplayExpenseComponent {
         this.expense = val;
         console.log(val);
       });
+  }
+
+  DelMovie(ex: any) {
+    this.DelId = ex.id;
+    return this.http
+      .delete(
+        'https://648a952117f1536d65e94eb1.mockapi.io/expense' + '/' + this.DelId
+      )
+      .subscribe((val: any) => {
+        this.getMovie();
+
+        //  this.movies=val;
+        //console.log(val);
+      });
+  }
+
+  UpdateExpense(id:any){
+    this.router.navigate(['updateexpense',id]);
+  }
+
+  AddExpense(name:string,amount:string,date:string,desc:string){
+    this.newmovie.name = name;
+    this.newmovie.amount = amount;
+    this.newmovie.date= date;
+    this.newmovie.desc = desc;
+    
+    this.transService.createExpense(this.newmovie);
+    this.getMovie();
+  
+
+  }
+
+  getMovie(){
+
+    this.http
+      .get('https://648a952117f1536d65e94eb1.mockapi.io/expense')
+      .subscribe((val: any) => {
+        this.expense = val;
+        console.log(val);
+      });
+
+
   }
 
 }
